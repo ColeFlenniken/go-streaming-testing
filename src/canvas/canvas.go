eeae0ede465ae1a87159136d693f296de771fa15
cole <f8cole@gmail.com>
2025-03-22 17:05:10 -0400

    build out canvas api

--- src/canvas/canvas.go
+++ src/canvas/canvas.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+// TODO merge canvas and Managed Canvas. Build perhaps using an interface. This will allow managed canvas to has a array of pixels
+// with associated timestamps that helps on sync. Note this should not get serialized (unless specs change) as the times are just server side
 type ManagedCanvas struct {
 	canvas Canvas
 	ts     time.Time
@@ -23,18 +25,33 @@ type CanvasDelta struct {
 	Color byte
 }
 
+func NewCanvas(height uint, width uint) (Canvas, error) {
+	if height > 1<<12 || width > 1<<12 {
+		return Canvas{}, fmt.Errorf("invalid creation dimensions: height and width may be 4096 at maximum")
+	}
+	return Canvas{Height: height, Width: width, Pixels: make([]byte, height*width)}, nil
+}
+
 func (mCanvas *ManagedCanvas) Update(deltas []CanvasDelta) error {
+	if deltas == nil {
+		return fmt.Errorf("deltas cannot be nil")
+	}
 	var canvas = mCanvas.canvas
-	if len(canvas.Pixels) != int(canvas.Height*canvas.Width) {
-		fmt.Errorf("Error: number of pixels in canvas is %v, while width*height of canvas is %v", len(canvas.Pixels), canvas.Height*canvas.Width)
+	expectedPixels := int(canvas.Height * canvas.Width)
+	if len(canvas.Pixels) != expectedPixels {
+		return fmt.Errorf("invalid canvas dimensions: got %d pixels, expected %d",
+			len(canvas.Pixels), expectedPixels)
 	}
 	var len = len(deltas)
 	for i := 0; i < len; i++ {
-		if deltas[i].Y < 0 || deltas[i].Y >= canvas.Height {
-			return fmt.Errorf("Error: Y value of delta out of bounds. Height of Canvas is %v, while the Y of the delta of index %v is %v", canvas.Height, i, deltas[i].Y)
+		if deltas[i].Y >= canvas.Height {
+			return fmt.Errorf("y value of delta out of bounds. Height of Canvas is %v, while the Y of the delta of index %v is %v", canvas.Height, i, deltas[i].Y)
 		}
-		if deltas[i].X < 0 || deltas[i].X >= canvas.Width {
-			return fmt.Errorf("Error: x value of delta out of bounds. Width of Canvas is %v, while the Y of the delta of index %v is %v", canvas.Height, i, deltas[i].X)
+		if deltas[i].X >= canvas.Width {
+			return fmt.Errorf("x value of delta out of bounds. Width of Canvas is %v, while the Y of the delta of index %v is %v", canvas.Height, i, deltas[i].X)
+		}
+		if deltas[i].Color >= 8 {
+			return fmt.Errorf("color value must be in the range 0-7")
 		}
 	}
 	//seperate loop is a convinience that prevents rollback logic. Can be changed later for improved efficiency assuming rate if bounds errors is low
@@ -43,4 +60,8 @@ func (mCanvas *ManagedCanvas) Update(deltas []CanvasDelta) error {
 	}
 	mCanvas.ts = time.Now()
 	return nil
+}
+
+func (mCanvas *ManagedCanvas) Get() Canvas {
+	return mCanvas.canvas
 }
