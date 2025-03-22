3e1707fe5dae8c5c95cbeefdb8d3aad7d0773457
cole <f8cole@gmail.com>
2025-03-21 20:03:13 -0400

    refactor to add bitCursor

--- src/api/canvasApi.go
+++ src/api/canvasApi.go
@@ -1,9 +1,26 @@
 package api
 
+type CanvasDelta struct {
+	x     uint
+	y     uint
+	color byte
+}
 type Canvas struct {
 	width  uint
 	height uint
 	pixels []byte
+}
+type BitCursor struct {
+	byteN int
+	bitN  int
+}
+
+func (place *BitCursor) Increment() {
+	place.bitN += 1
+	if place.bitN == 8 {
+		place.byteN += 1
+		place.bitN = 0
+	}
 }
 
 /*
@@ -44,31 +61,16 @@ func Serialize(canvas Canvas) []byte {
 	output[1] = byte(canvas.height&0b00001111)<<4 | byte(canvas.width>>4)
 	output[2] = byte(canvas.width&0b00001111) << 4
 	//for each pixel add 3 bit color code
-	var byteN = 3 // start at 3 to account for size header
-	var bitN = 0
+	var place BitCursor = BitCursor{byteN: 3, bitN: 0}
 	for i := 0; i < len(canvas.pixels); i++ {
-		output[byteN] = (output[byteN] << 1) | (canvas.pixels[i] >> 2)
-		Increment(&byteN, &bitN)
-		output[byteN] = (output[byteN] << 1) | (canvas.pixels[i] >> 1)
-		Increment(&byteN, &bitN)
-		output[byteN] = (output[byteN] << 1) | (canvas.pixels[i])
-		Increment(&byteN, &bitN)
+		output[place.byteN] = (output[place.byteN] << 1) | (canvas.pixels[i] >> 2)
+		place.Increment()
+		output[place.byteN] = (output[place.byteN] << 1) | (canvas.pixels[i] >> 1)
+		place.Increment()
+		output[place.byteN] = (output[place.byteN] << 1) | (canvas.pixels[i])
+		place.Increment()
 	}
 	return output
-}
-
-func Increment(byteN *int, bitN *int) {
-	*bitN += 1
-	if *bitN == 8 {
-		*byteN += 1
-		*bitN = 0
-	}
-}
-
-type CanvasDelta struct {
-	x     uint
-	y     uint
-	color byte
 }
 
 /*
@@ -97,57 +99,57 @@ func DeltaSerialize(changes []CanvasDelta) []byte {
 	output[0] = byte(numChanges >> 16)
 	output[1] = byte(numChanges >> 8)
 	output[2] = byte(numChanges)
-	var byteN int = 3
-	var bitN int = 0
+	var place BitCursor = BitCursor{byteN: 3, bitN: 0}
 	for i := 0; i < numChanges; i++ {
-		byteN, bitN = PackDelta(&output, byteN, bitN, changes[i])
+		PackDelta(output, &place, changes[i])
 	}
 	return output
 }
 
-func PackDelta(buff *[]byte, byteN int, bitN int, delta CanvasDelta) (int, int) {
+func PackDelta(buff []byte, place *BitCursor, delta CanvasDelta) {
 	//This seems quite inefficient. Perhaps can get the next x bits where
 	// x is the min of bits left in byte and number of bits left in x/y/color in the delta
 	for i := 0; i < 12; i++ {
-		(*buff)[byteN] = (*buff)[byteN]<<1 | byte(delta.y>>(11-i))
-		Increment(&byteN, &bitN)
+		buff[place.byteN] = buff[place.byteN]<<1 | byte(delta.y>>(11-i))
+		place.Increment()
 	}
 	for i := 0; i < 12; i++ {
-		(*buff)[byteN] = (*buff)[byteN]<<1 | byte(delta.x>>(11-i))
-		Increment(&byteN, &bitN)
+		buff[place.byteN] = buff[place.byteN]<<1 | byte(delta.x>>(11-i))
+		place.Increment()
 	}
 	for i := 0; i < 3; i++ {
-		(*buff)[byteN] = (*buff)[byteN]<<1 | byte(delta.color>>(2-i))
-		Increment(&byteN, &bitN)
+		buff[place.byteN] = buff[place.byteN]<<1 | byte(delta.color>>(2-i))
+		place.Increment()
 	}
-	return byteN, bitN
+
 }
 
 func DeltaDeserialize(changes []byte) []CanvasDelta {
 	var numChanges int = int(changes[0])<<16 | int(changes[1])<<8 | int(changes[2])
 	var output []CanvasDelta = make([]CanvasDelta, numChanges)
-	var byteN int = 3
-	var bitN int = 0
+	var place BitCursor = BitCursor{byteN: 3, bitN: 0}
+
 	for i := 0; i < numChanges; i++ {
-		output[i] = deltaDeseralizeSingle(changes, &byteN, &bitN)
+		output[i] = deltaDeseralizeSingle(changes, &place)
 	}
 	return output
 }
-func deltaDeseralizeSingle(changes []byte, byteN *int, bitN *int) CanvasDelta {
+func deltaDeseralizeSingle(changes []byte, place *BitCursor) CanvasDelta {
 	var delta = CanvasDelta{}
 	for i := 0; i < 12; i++ {
 		delta.y <<= 1
-		delta.y |= uint(changes[*byteN]>>7 - byte(*bitN))
-		Increment(byteN, bitN)
+		delta.y |= uint(changes[place.byteN] >> (7 - byte(place.bitN)))
+		place.Increment()
 	}
 	for i := 0; i < 12; i++ {
 		delta.x <<= 1
-		delta.x |= uint(changes[*byteN]>>7 - byte(*bitN))
-		Increment(byteN, bitN)
+		delta.x |= uint(changes[place.byteN] >> (7 - byte(place.bitN)))
+		place.Increment()
 	}
 	for i := 0; i < 3; i++ {
 		delta.color <<= 1
-		delta.color |= changes[*byteN]>>7 - byte(*bitN)
-		Increment(byteN, bitN)
+		delta.color |= changes[place.byteN]>>7 - byte(place.bitN)
+		place.Increment()
 	}
+	return delta
 }
