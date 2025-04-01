715860d8393c951fa9f2dab9f215ff51a566e8cb
cole <f8cole@gmail.com>
2025-04-01 14:46:38 -0400

    add api functions

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
@@ -11,7 +12,7 @@ import (
 	"github.com/go-streaming-testing/src/canvas"
 )
 
-//shared state
+// shared state
 var mCanvas canvas.ManagedCanvas
 
 func updateData(w http.ResponseWriter, r *http.Request) {
@@ -23,24 +24,35 @@ func updateData(w http.ResponseWriter, r *http.Request) {
 	mCanvas.Update(deser)
 }
 
-func getData(w http.ResponseWriter, r *http.Request){
-	body, err := io.ReadAll(r.)
+func getData(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal("issue reading from body")
 	}
-	
+	changeId, err := strconv.Atoi(string(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	deltas, err := mCanvas.GetChanges(changeId)
+	if err != nil {
+		canvasFull := canvas.Serialize(mCanvas.GetCanvas())
+		w.Write(append([]byte{byte(0)}, canvasFull...))
+	}
+	canvasDeltas := canvas.DeltaSerialize(deltas)
+	w.Write(append([]byte{1}, canvasDeltas...))
 }
 func main() {
 	canvasData, err := canvas.NewCanvas(500, 500)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	mCanvas = canvas.ManagedCanvas{M : sync.Mutex{}, ChangeLog: canvas.MakeCircularArray(1000), Ts: time.Now(), Id: 1, Canvas: canvasData}
+
+	mCanvas = canvas.ManagedCanvas{M: sync.Mutex{}, ChangeLog: canvas.MakeCircularArray(1000), Ts: time.Now(), Id: 1, Canvas: canvasData}
 
 	mux := http.NewServeMux()
 	component := Index()
 	mux.HandleFunc("/update", updateData)
+	mux.HandleFunc("/getData", getData)
 	mux.Handle("/", templ.Handler(component))
 
 	err = http.ListenAndServe(":3333", mux)
